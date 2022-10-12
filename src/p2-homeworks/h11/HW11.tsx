8260ae69ff640a2edbb60cc2c1ed4af1a97a5634
Pavel-Solovev <pavelsol1990@gmail.com>
2022-10-12 18:40:54 +0300

    Complete HW 11 and almost complete 11*

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,17 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+    const min = 0
+    const max = 100
+
+    const onChange = useCallback((value: [number, number]) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }, [])
 
     return (
         <div>
@@ -15,6 +22,11 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={setValue1}
+                    min={min}
+                    max={max}
+
+
                     // сделать так чтоб value1 изменялось
                 />
             </div>
@@ -22,9 +34,10 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+                    min={min}
+                    max={max}
+                    onChange={onChange}
                 />
-                <span>{value2}</span>
             </div>
 
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,98 @@
-import React from 'react'
+import React, {
+    ChangeEvent,
+    useCallback,
+    useEffect,
+    useRef,
+    useState
+} from 'react'
+import "./SuperDoubleRange.css";
+
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
+    onChange: (value: [number, number]) => void
     value?: [number, number]
-    // min, max, step, disable, ...
+    max: number
+    min: number
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+        onChange,
+        max, min,
+        value,
+        ...restProps
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+    const [maxValue, setMaxValue] = useState(max)
+    const [minValue, setMinValue] = useState(min)
+    const maxValRef = useRef(max)
+    const minValRef = useRef(min)
+    const range = useRef(document.querySelector('#range') as HTMLElement | null)
+
+    const getPercent = useCallback((value: number) => {
+        Math.round((value - min) / (max - min) * 100)
+    }, [min, max])
+
+    useEffect(() => {
+        const minPercent = getPercent(minValue)
+        const maxPercent = getPercent(maxValRef.current)
+
+        if (range.current) {
+            range.current.style.left = `${minPercent}%`;
+            range.current.style.width = `${Number(maxPercent) - Number(minPercent)}%`
+        }
+    }, [minValue, getPercent])
+
+    useEffect(() => {
+        const maxPercent = getPercent(maxValue)
+        const minPercent = getPercent(minValRef.current)
+
+        if (range.current) {
+            range.current.style.width = `${Number(maxPercent) - Number(minPercent)}`
+        }
+    }, [maxValue, getPercent])
+
+    useEffect(() => {
+        onChange && onChange([maxValue, minValue]);
+    }, [maxValue, minValue, onChange])
+
 
     return (
-        <>
-            DoubleRange
-        </>
+        <div className="container">
+            <input
+                type="range"
+                min={min}
+                max={max}
+                value={minValue}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    const value = Math.min(Number(e.target.value), maxValue - 1);
+                    setMinValue(value);
+                    minValRef.current = value;
+                }}
+                className="thumb thumb--right"
+            />
+            <input
+                type="range"
+                min={min}
+                max={max}
+                value={maxValue}
+                onChange={(event) => {
+                    const value = Math.max(Number(event.target.value), minValue + 1);
+                    setMaxValue(value);
+                    maxValRef.current = value;
+                }}
+                className="thumb thumb--right"
+            />
+            <div className="slider">
+                <div className="slider__track"/>
+                {/*<div ref={range} className="slider__range" />*/}
+                <div className="slider__left-value">{minValue}</div>
+                <div className="slider__right-value">{maxValue}</div>
+            </div>
+        </div>
+
     )
 }
+
 
 export default SuperDoubleRange
